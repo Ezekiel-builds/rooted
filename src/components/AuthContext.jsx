b4f96621e4be020f4a7c6748adb8a138f7c5030d
Ezekiel-builds <officialbuildwithzek@gmail.com>
2026-09-20 22:03:44 +0100

    feat(auth): complete user sign up flow with supabase profile creation

---
+++ src/components/AuthContext.jsx
@@ -0,0 +1,2 @@
+import { supabase } from "../supabaseClient";
+import { createContextc, useContext, useState, useEffect } from "react";
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,52 @@
-import Footer from "../components/Footer";
+import { supabase } from "../supabaseClient";
+import { useState } from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import google from '../assets/Google.png';
 import logoImg from '../assets/logo.png';
 import './SignUp.css';
 import { Link } from "react-router-dom";
 
 function SignUp() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [churchName, setChurchName] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
+    
+    async function handleSignUp(e) {
+        e.preventDefault();
+        setErrorMessage(null);
+
+        const {data, error} = await supabase.auth.signUp({
+            email,
+            password
+        });
+
+        if(error) {
+            setErrorMessage(error.message);
+            console.log(error.message);
+            return;
+        };
+
+        console.log(data);
+
+        const newUser = data.user;
+
+        const { error: profileError } = await supabase
+        .from('profiles')
+        .insert({
+            id: newUser.id,
+            full_name: fullName,
+            church_name: churchName
+        });
+
+        if(profileError) {
+            console.log(profileError.message);
+        } else {
+            console.log('Acoount created successfully');
+        }
+    }
     return (
         <>
 
@@ -27,25 +68,46 @@ function SignUp() {
                     </p>
                 </div>
 
-                <form className="signUp__form">
+                <form className="signUp__form" onSubmit={handleSignUp}>
                     <div className="form__meta">
                         <label htmlFor="Full name" className="form__label">Full Name</label>
-                        <input type="text" placeholder="Jane Doe" className="form__input" />
+                        <input 
+                        type="text" 
+                        placeholder="Jane Doe"
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)} 
+                        className="form__input" 
+                        />
                     </div>
 
                     <div className="form__meta">
                         <label htmlFor="Church name" className="form__label">Church Name<span>(Optional)</span></label>
-                        <input type="text" placeholder="Grace Community Church" className="form__input" />
+                        <input 
+                        type="text" 
+                        placeholder="Grace Community Church" 
+                        value={churchName}
+                        onChange={(e) => setChurchName(e.target.value)}
+                        className="form__input" />
                     </div>
 
                     <div className="form__meta">
                         <label htmlFor="Full name" className="form__label">Email Address</label>
-                        <input type="email" placeholder="Jane@example.com" className="form__input" />
+                        <input 
+                        type="email" 
+                        placeholder="Jane@example.com" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form__input" />
                     </div>
 
                     <div className="form__meta">
                         <label htmlFor="Full name" className="form__label">Password</label>
-                        <input type="password" placeholder="••••••••" className="form__input" />
+                        <input 
+                        type="password" 
+                        placeholder="••••••••" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="form__input" />
                     </div>
 
                 <div className="form__bottom">
